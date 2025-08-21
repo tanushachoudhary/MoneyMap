63daca6c8b977a7ea7e7c18ff0ac5623170eb063
Tanusha <tanushachoudhary18@gmail.com>
2025-08-21 16:11:57 +0530

    feat: implement income management with CRUD operations and file upload functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,11 +58,24 @@ exports.loginUser = async (req, res) => {
       token: generateToken(user._id),
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error registering user", error: error.message });
+    res.status(500).json({ message: "Error logging in", error: error.message });
   }
 };
 
 //get user info
-exports.getUserInfo = async (req, res) => {};
+exports.getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Error getting user information",
+        error: error.message,
+      });
+  }
+};
