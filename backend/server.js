bb4a6069896e27dce8715f06bf765262f2be47d5
Tanusha <tanushachoudhary18@gmail.com>
2025-08-21 21:19:57 +0530

    feat: implement expense management with CRUD operations and Excel download functionality

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
 
 const app = express();
 app.use(
@@ -24,6 +25,7 @@ connectDB();
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
 
 //serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
